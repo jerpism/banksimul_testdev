5f08198f2f9c2747107e6a623fc2c455e324f5bb
Jerpism <jere.m@live.fi>
2021-03-30 13:36:18 +0300

    Add and test routes and models for withdrawing and transfering money

---
+++ restapi/routes/actions.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const actions  = require('../models/actions_model');
+
+
+router.post('/withdraw_action',
+    function(request, response){
+        actions.withdrawAction(request.body, function(err, dbResult){
+            if(err){
+                response.json(err);
+             }else{
+                 response.json(dbResult.affectedRows);
+             }
+        });
+    });
+
+router.post('/transfer_action',
+    function(request, response){
+        actions.transferAction(request.body, function(err, dbResult){
+            if(err){
+                response.json(err);
+            }else{
+                response.json(dbResult.affectedRows);
+            }
+        });
+    });
+
+module.exports = router;
+
+
+
+
