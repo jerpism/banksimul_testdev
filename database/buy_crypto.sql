e769d54cf888957ca4e130c9c1cf30baa573bcf2
Jerpism <jere.m@live.fi>
2021-04-13 13:49:25 +0300

    Change cryptocurrency related stored procedures to use currency rates stored in the database

--- database/buy_crypto.sql
+++ database/buy_crypto.sql
@@ -14,20 +14,22 @@ buy_crypto:BEGIN
 
  -- SELECT cryptoaccount.id_cryptoaccount INTO @cryptoacc FROM card JOIN cryptoaccount ON card.id_cryptoaccount = cryptoaccount.id_cryptoaccount WHERE id_card = cardid;
  --   SELECT account.id_account INTO @accid FROM card JOIN account ON card.id_account = account.id_account WHERE id_card = cardid;
+ 
+	SELECT crypto INTO @rate FROM rates;
 
 	START transaction;
     UPDATE account SET balance = balance-amount WHERE id_account = id_acc AND balance-amount > 0;
     SET @test=row_count();
     
     IF(@test > 0) THEN
-		UPDATE cryptoaccount SET balance = balance+amount*0.005 WHERE id_cryptoaccount = id_crypto;
+		UPDATE cryptoaccount SET balance = balance+amount*@rate WHERE id_cryptoaccount = id_crypto;
         
         SET @test=row_count();
         
         IF(@test > 0) THEN
 			COMMIT;
             INSERT INTO account_action(id_account,amount,date,action_type) VALUES(id_acc,-amount,NOW(),'Kryptovaluutan osto');
-            INSERT INTO cryptoaccount_action (id_cryptoaccount,amount,date,action_type) VALUES(id_crypto,+amount*0.005,NOW(),'Kryptovaluutan osto');
+            INSERT INTO cryptoaccount_action (id_cryptoaccount,amount,date,action_type) VALUES(id_crypto,+amount*@rate,NOW(),'Kryptovaluutan osto');
 		ELSE
 			ROLLBACK;
             END IF;
