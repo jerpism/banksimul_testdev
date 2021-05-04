e13cb4a7082917ff072a1660bac7d5f96bf02449
j <j@lynne.koti>
2021-05-04 12:47:03 +0300

    Add check for locked accounts to /login

--- restapi/routes/login.js
+++ restapi/routes/login.js
@@ -8,24 +8,43 @@ router.post('/',
     if(request.body.id && request.body.pin){
       const id = request.body.id;
       const pin = request.body.pin;
-        login.checkPin(id, function(dbError, dbResult) {
+
+    /*  login.isLocked(id, function(dbError, dbResult){
           if(dbError){
-            response.json(dbError);
+              response.json(dbError);
           }
-          else{
+          if(dbResult.length > 0){
+              if(dbResult[0].locked == 1){
+                  console.log("Tili on lukittu");
+                  response.send("locked");
+                  response.end();
+              }
+          }
+      }
+      );*/
+     
+     login.checkPin(id, function(dbError, dbResult) {
+         if(dbError){
+             response.json(dbError);
+          }else{
             if (dbResult.length > 0) {
+
+            if(dbResult[0].locked == 1){
+                response.send("locked");
+            }else{
               bcrypt.compare(pin,dbResult[0].pin, function(err,compareResult) {
                 if(compareResult) {
-                  console.log("succes");
-                  response.send(true);
-                }
-                else {
+                    console.log("succes");
+                    response.send(true);
+                }else{
                     console.log("wrong pin");
                     response.send(false);
                 }			
                   response.end();
               }
+            
               );
+            }
             }
             else{
               console.log("user does not exists");
@@ -35,11 +54,13 @@ router.post('/',
           }
         );
       }
+          
     else{
       console.log("id or pin missing");
       response.send(false);
     }
   }
 );
+  
 
 module.exports=router;
