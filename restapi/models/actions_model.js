881249713e8182eeb785581d38d817bc39af5b19
j <j@lynne.koti>
2021-05-05 12:07:01 +0300

    Add limiting to between 2 ints in actions

--- restapi/models/actions_model.js
+++ restapi/models/actions_model.js
@@ -3,15 +3,18 @@ const db = require('../database');
 const actions = {
 
     getRecent: function(id, callback){
-        return db.query('SELECT amount,convert(date, varchar(45)) as date,action_type FROM account_action JOIN account ON account.id_account = account_action.id_account where account.id_account = ? order by date DESC LIMIT 10',[id],callback);
+        return db.query('SELECT amount,convert(date, varchar(45)) as date,action_type FROM account_action JOIN account ON account.id_account = account_action.id_account where account.id_account = ? order by date DESC',[id],callback);
 
     },
 
     getRecentLimit: function(id, limit, callback){
         return db.query('SELECT amount,convert(date, varchar(45)) as date, action_type FROM account_action JOIN account ON account.id_account = account_action.id_account where account.id_account = ? order by date DESC LIMIT ?',[id,limit],callback);
+    },
+
+    getRecentLimitBetween: function(id, limit, limit_b, callback){
+        return db.query('SELECT amount,convert(date, varchar(45)) as date, action_type FROM account_action JOIN account ON account.id_account = account_action.id_account where account.id_account = ? order by date DESC LIMIT ?,?',[id,limit,limit_b],callback);
     }
 };
-
 
 module.exports = actions;
 
